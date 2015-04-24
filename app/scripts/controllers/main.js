3a5be7653660ecb8541f0ac1f34b8a1252394cc0
Fabian Meyer <fabian.meyer@studiointeractive.de>
2015-04-24 14:50:29 +0200

    First very basic calculations

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,20 @@
  * Controller of the dprApp
  */
 angular.module('dprApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+
+        $scope.difficulty = 15;
+        $scope.attack = 10;
+
+        $scope.damageDiceSideOptions = ['2','4', '6', '8', '10', '12','20', '100'];
+
+        $scope.damageDiceCount = 1;
+        $scope.damageDiceSides = 6;
+        $scope.damageDiceBonus = 2;
+
+    });
